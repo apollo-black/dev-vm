5acb95d24abc4212b10e789a27c1995648dd6313
Sean Nieuwoudt <sean@isean.co.za>
2019-02-28 10:12:23 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,210 @@
-# dev-vm
-The Apollo Development VM
+# Apollo Development VM
+
+This Virtual Machine has everything installed that we use to develop software. It's the single source of runtime that imitates the production servers that we use.
+
+## Usage
+
+Steps:
+1 Launch image with virtualbox
+2. install git
+3 clone repo
+4. run set up
+
+run: sudo mysql_secure_installation
+
+
+1. Copy the `Vagrantfile` from this repository into your project root
+2. Run `vagrant up`
+
+
+## Dependencies
+
+- [VirtualBox](https://www.virtualbox.org) `tested on v5.2.16`
+- [Vagrant](https://www.vagrantup.com) `tested on v2.1.5`
+
+## Installed Components
+
+#### Programming Stacks
+
+- [Nodejs 10.7.0](https://nodejs.org/en/)
+- [NVM 0.33.11](https://github.com/creationix/nvm)
+- [NPM 6.1.0](https://www.npmjs.com/)
+- [PHP 7.0](http://php.net/)
+- [Ruby 2.6.1](https://www.ruby-lang.org/en/)
+- [Bundler 1.16.3](https://bundler.io/)
+- [Webpacker 3.5.5](https://github.com/rails/webpacker)
+- [Crystal 0.25.1](https://crystal-lang.org/)
+- [Java 8](https://java.com/en/download/)
+- [Python 2.7 & 3.5.2](https://www.python.org/)
+- [Lua 5.3.5](https://www.lua.org/)
+
+#### Databases
+
+- [Postgres 10](https://www.postgresql.org/)
+- [Redis 4.0.10](https://redis.io/)
+- [Cassandra 3.11](http://cassandra.apache.org/)
+- [MySQL 8.0](https://www.mysql.com/)
+
+All database usernames and passwords would generally be:
+
+- Username: vagrant
+- Password: vagrant
+
+## Machine Learning
+
+- [Tensorflow 1.10](https://www.tensorflow.org)
+- [Keras 2.2.2](https://keras.io)
+
+#### Other Tools
+
+- [AWS CLI Tools](https://aws.amazon.com/cli/)
+- [Heroku Command Line v7.7.4](https://devcenter.heroku.com/articles/heroku-cli)
+- [Docker]()
+- [Vue CLI]()
+- [React Native CLI]()
+- [Yarn]()
+- [Expo CLI]()
+- [Tesseract]()
+
+#### Utilities
+
+There are utilities installed on the machine to help make life easier:
+
+To share your environment using [ngrok](https://ngrok.com):
+
+```
+ngrok http 3000
+```
+
+[is-up 1.0](https://github.com/sindresorhus/is-up-cli): Check if a website is up or down.
+
+```
+is-up apollo.black
+```
+
+[pageres-cli 4.1.0](https://github.com/sindresorhus/pageres-cli): Capture screenshots of websites in any resolution
+
+```
+pageres apollo.black 1024x768
+```
+
+[loadtest 3.0.3](https://github.com/alexfernandez/loadtest): Perform a load test on any URL
+
+```
+loadtest -n 100 -c 5 -k apollo.black
+```
+
+[pgweb 0.9.12](https://github.com/sosedoff/pgweb): Plug and play Postgresql web interface
+
+```
+pgweb --host localhost --user app_user --db app_db
+```
+
+[htop](https://hisham.hm/htop/): Interactive unix process viewer
+
+```
+htop
+```
+
+[httpie](https://httpie.org/): Command line HTTP client
+
+```
+http -v apollo.black
+```
+
+## Useful Vagrant Commands
+
+Daily use sometimes requires that you destroy or reload virtual machines, here are some useful commands.
+
+##### Launch a VM
+```
+vagrant up
+```
+
+##### Connect to VM via SSH
+```
+vagrant ssh
+```
+
+##### Stop a VM
+```
+vagrant halt
+```
+
+##### Reload a VM without needing to destroy it
+```
+vagrant reload
+```
+
+##### Destroy a VM
+```
+vagrant destroy
+```
+
+##### Show VM's & Statuses
+```
+vagrant global-status
+```
+
+## Environment Variables
+
+To make life easier, we have the following ENV variables defined in `/etc/environment`:
+
+```
+> DATABASE_URL="postgresql://vagrant:vagrant@127.0.0.1:5432/vagrant"
+> REDIS_URL="redis://localhost:6379"
+> RAILS_ENV="development"
+```
+
+You can add new ENV vars to either the `/etc/environment` file or the `~/.profile` file.
+
+If you install [Nginx](https://www.nginx.com/) and [Phusion Passenger](https://www.phusionpassenger.com/) on the VM and want to set application specific `ENV` variables, you can use [passenger_env_var](https://www.phusionpassenger.com/library/config/nginx/reference/#passenger_env_var) directive:
+
+```
+> passenger_env_var DATABASE_URL postgresql://vagrant:vagrant@127.0.0.1:5432/vagrant;
+> passenger_env_var REDIS_URL redis://localhost:6379;
+```
+Similarly, with [Apache2](https://httpd.apache.org/), you can use the [SetEnv](https://httpd.apache.org/docs/2.4/mod/mod_env.html) directive:
+
+```
+> SetEnv DATABASE_URL postgresql://vagrant:vagrant@127.0.0.1:5432/vagrant
+> SetEnv REDIS_URL redis://localhost:6379
+```
+
+## Memory / VRAM
+
+You can adjust the available VM memory within the `Vagrantfile`, by changing `vb.memory`:
+
+```
+config.vm.provider "virtualbox" do |vb|
+  vb.memory = "2048"
+end
+```
+
+Any number not exceeding your available host RAM will work.
+
+**Note:** Depending on what development frameworks you want to use, you'll need to fiddle with the available VRAM for your VM instance. Java and .Net(Mono), BigData or ML/AI based projects will require much more VRAM than simpler tech like PHP or Ruby.
+
+## License
+
+MIT License
+
+Copyright (c) 2019 Apollo Black
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ setup.sh
@@ -0,0 +1,230 @@
+echo "# -------------------------------- #"
+echo "#      Apollo Ruby VM Install      #"
+echo "# -------------------------------- #"
+
+mkdir /home/vagrant/downloads
+
+# Add vagrant to sudoers
+sudo echo "vagrant ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/vagrant
+
+# Install general dependencies
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get -y install wget ca-certificates gcc g++ gnupg2 make software-properties-common git-core curl build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev libcurl4-openssl-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libffi-dev libpq-dev tcl8.5 libexpat1-dev gettext unzip  libmagick++-dev libv8-dev libffi-dev libpulse0
+
+echo "# -------------------------------- #"
+echo "#         Setting SSH Keys         #"
+echo "# -------------------------------- #"
+
+mkdir -p /home/vagrant/.ssh
+chmod 0700 /home/vagrant/.ssh
+mv ./ssh/vagrant.pub /home/vagrant/.ssh/authorized_keys
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+sudo chown -R vagrant:root /home/vagrant/.ssh
+sudo apt-get install -y openssh-server
+echo 'AuthorizedKeysFile %h/.ssh/authorized_keys' | sudo tee --append /etc/ssh/sshd_config > /dev/null
+sudo service ssh restart
+
+echo "# -------------------------------- #"
+echo "#    Installing Guest Additions    #"
+echo "# -------------------------------- #"
+
+sudo mount /dev/cdrom /media/cdrom
+cd /media/cdrom
+sudo apt-get install -y dkms build-essential linux-headers-generic linux-headers-$(uname -r)
+sudo su
+./VBoxLinuxAdditions.run
+exit
+
+echo "# -------------------------------- #"
+echo "#         Installing Ruby          #"
+echo "# -------------------------------- #"
+
+echo "gem: --no-ri --no-rdoc" > ~/.gemrc
+cd /home/vagrant/downloads && wget http://cache.ruby-lang.org/pub/ruby/2.6/ruby-2.6.1.tar.gz -O ruby.tar.gz
+cd /home/vagrant/downloads && tar xzf ruby.tar.gz
+cd /home/vagrant/downloads/ruby-2.6.1 && ./configure -prefix=$HOME
+cd /home/vagrant/downloads/ruby-2.6.1 && make
+cd /home/vagrant/downloads/ruby-2.6.1 && make install
+gem install bundler
+
+echo "# -------------------------------- #"
+echo "#         Installing Redis         #"
+echo "# -------------------------------- #"
+
+cd /home/vagrant/downloads && wget http://download.redis.io/releases/redis-stable.tar.gz -O redis.tar.gz
+cd /home/vagrant/downloads && tar xzf redis.tar.gz
+cd /home/vagrant/downloads/redis-stable && make
+cd /home/vagrant/downloads/redis-stable && sudo make install
+cd /home/vagrant/downloads/redis-stable/utils && sudo ./install_server.sh
+
+echo "# -------------------------------- #"
+echo "#       Installing PostgreSQL      #"
+echo "# -------------------------------- #"
+
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+sudo apt-get update
+sudo apt-get -y install postgresql postgresql-contrib
+
+echo "# -------------------------------- #"
+echo "#         Setting up DB User       #"
+echo "# -------------------------------- #"
+
+sudo -u postgres psql -c "CREATE USER vagrant WITH PASSWORD 'vagrant';"
+sudo -u postgres createdb -O vagrant vagrant
+
+echo "# -------------------------------- #"
+echo "#        Installing Java 8         #"
+echo "# -------------------------------- #"
+
+sudo apt install default-jre default-jre-headless
+
+echo "# -------------------------------- #"
+echo "#        Installing Neo4j          #"
+echo "# -------------------------------- #"
+
+wget --no-check-certificate -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
+echo 'deb http://debian.neo4j.org/repo stable/' | sudo tee /etc/apt/sources.list.d/neo4j.list
+sudo apt update
+sudo apt install -y neo4j
+sudo service neo4j stop
+sudo service neo4j start
+
+echo "# -------------------------------- #"
+echo "#        Installing Nodejs         #"
+echo "# -------------------------------- #"
+
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt install -y npm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+npm install --global webpack
+
+echo "# -------------------------------- #"
+echo "#        Installing Expo           #"
+echo "# -------------------------------- #"
+
+sudo npm install -g expo-cli
+
+echo "# -------------------------------- #"
+echo "#      Installing React Native     #"
+echo "# -------------------------------- #"
+
+sudo npm install -g react-native-cli
+
+echo "# -------------------------------- #"
+echo "#         Installing VueCLI        #"
+echo "# -------------------------------- #"
+
+sudo npm install -g vue-cli
+
+echo "# -------------------------------- #"
+echo "#        Installing Crystal        #"
+echo "# -------------------------------- #"
+
+curl -sSL https://dist.crystal-lang.org/apt/setup.sh | sudo bash
+sudo apt install -y crystal
+
+echo "# -------------------------------- #"
+echo "#        Installing PHP            #"
+echo "# -------------------------------- #"
+
+sudo apt install -y curl php-cli php-mbstring git unzip
+sudo apt install -y php
+sudo apt install -y php-mysql php-gd
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
+echo "# -------------------------------- #"
+echo "#          Installing Lua          #"
+echo "# -------------------------------- #"
+
+sudo apt-get install -y lua5.3
+
+echo "# -------------------------------- #"
+echo "#       Installing Cassandra       #"
+echo "# -------------------------------- #"
+
+echo "deb http://www.apache.org/dist/cassandra/debian 36x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y cassandra
+
+echo "# -------------------------------- #"
+echo "#         Installing MySQL         #"
+echo "# -------------------------------- #"
+
+sudo apt install -y mysql-server
+
+echo "# -------------------------------- #"
+echo "#         Installing PIP           #"
+echo "# -------------------------------- #"
+
+sudo apt install -y python3-pip
+
+echo "# -------------------------------- #"
+echo "#      Installing Tensorflow       #"
+echo "# -------------------------------- #"
+
+pip3 install tensorflow
+pip3 install keras
+
+echo "# -------------------------------- #"
+echo "#      Installing Tesseract        #"
+echo "# -------------------------------- #"
+
+sudo apt install -y tesseract-ocr
+sudo apt install -y libtesseract-dev
+
+echo "# -------------------------------- #"
+echo "#         Installing FFmpeg        #"
+echo "# -------------------------------- #"
+
+sudo apt install -y ffmpeg
+
+echo "# -------------------------------- #"
+echo "#       Installing Typescript      #"
+echo "# -------------------------------- #"
+
+sudo npm install --global typescript
+
+echo "# -------------------------------- #"
+echo "#          Setting ENV Vars        #"
+echo "# -------------------------------- #"
+
+echo 'RAILS_ENV="development"' | sudo tee --append /etc/environment > /dev/null
+echo 'DATABASE_URL="postgresql://vagrant:vagrant@127.0.0.1:5432/vagrant"' | sudo tee --append /etc/environment > /dev/null
+echo 'REDIS_URL="redis://localhost:6379"' | sudo tee --append /etc/environment > /dev/null
+
+echo "# -------------------------------- #"
+echo "#          Setting Up Utils        #"
+echo "# -------------------------------- #"
+
+cp ./utils/ngrok /usr/bin
+cp ./utils/pgweb /usr/bin
+
+sudo npm install -g is-up-cli
+sudo npm install -g pageres-cli
+sudo npm install -g loadtest
+
+sudo apt-get install -y htop
+sudo apt-get install -y httpie
+
+echo "# -------------------------------- #"
+echo "#        Installing Docker         #"
+echo "# -------------------------------- #"
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "# -------------------------------- #"
+echo "#         Done, Rebooting          #"
+echo "# -------------------------------- #"
+
+sudo reboot
