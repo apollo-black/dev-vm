0153c7139a7e4aaa4e1ad636bf51bba4a9501834
Sean Nieuwoudt <sean@isean.co.za>
2019-03-03 12:48:22 +0200

    Fixes

--- setup.sh
+++ setup.sh
@@ -100,6 +100,16 @@ sudo apt install -y npm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 npm install --global webpack
 
+## Run `sudo apt-get install -y nodejs` to install Node.js 10.x and npm
+## You may also need development tools to build native addons:
+     sudo apt-get install gcc g++ make
+## To install the Yarn package manager, run:
+     curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+     sudo apt-get update && sudo apt-get install yarn
+
+
+
 echo "# -------------------------------- #"
 echo "#        Installing Expo           #"
 echo "# -------------------------------- #"
